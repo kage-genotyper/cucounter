3a2e761cefd0ffb0ddee20ff6bc05e448a326560
jorgenwh <jorgenwhen@gmail.com>
2022-11-15 02:24:36 +0100

    github actions with cuda

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -1,12 +1,8 @@
 import numpy as np
 #import cupy as cp
 
-#from cucounter import Counter
+from cucounter import Counter
 
-def test_test():
-    assert np.all(np.zeros(10) == np.zeros(10))
-
-"""
 def test_counter_with_numpy():
     keys = np.array([0, 1, 2, 5, 10, 42], dtype=np.uint64)
     kmers = np.array([42, 0, 0, 0, 1, 5, 2, 2, 0, 1, 10, 42], dtype=np.uint64)
@@ -17,6 +13,7 @@ def test_counter_with_numpy():
 
     assert np.all(counts == np.array([4, 2, 2, 1, 1, 2], dtype=np.uint64))
 
+"""
 def test_counter_with_cupy():
     keys = cp.array([0, 1, 2, 5, 10, 42], dtype=cp.uint64)
     kmers = cp.array([42, 0, 0, 0, 1, 5, 2, 2, 0, 1, 10, 42], dtype=cp.uint64)
